60eb8e61af85d9fd2f0f395ef8cb16ed473899dd
theRealCooller <jakublohnicky@hotmail.com>
2020-05-09 17:03:57 +0200

    small refactor

--- src/Notification.php
+++ src/Notification.php
@@ -3,5 +3,5 @@ declare(strict_types=1);
 
 interface Notification
 {
-    public function alertStatus(): void;
+    public function notify(): void;
 }
--- src/StdNotification.php
+++ src/StdNotification.php
@@ -3,7 +3,7 @@ declare(strict_types=1);
 
 class StdNotification implements Notification
 {
-    public function alertStatus(): void
+    public function notify(): void
     {
         echo "Your connection is granted again from: " . date('d.m.Y - H:i', time());
     }
